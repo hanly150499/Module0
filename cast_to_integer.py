e5d67c6cabcccc84c966382a8782c791981c093b
lymyh <hly1@dmacc.edu>
2022-01-19 21:36:47 -0600

    Initial Commit

---
+++ cast_to_integer.py
@@ -0,0 +1,11 @@
+"""Author:Han Ly"""
+"Last date modified:01/19/2022"
+#Input   Expected Output    Actual Output
+#8.3        8                   8
+#321        32.1                321.0
+x=int(8.3)
+print(x)
+y=float(321)
+print(y)
+b=str(34.2)+str("bike")
+print(b)
